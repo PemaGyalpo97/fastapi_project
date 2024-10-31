783543ddf86c8cc5d0f64d93369ad11116b145c6
Pema Gyalpo <pemagyalpo74@gmail.com>
2024-10-31 15:51:10 +0600

    Done without UI

--- main.py
+++ main.py
@@ -40,14 +40,50 @@ def get_users():
         "data": records
     }
 
+# Define get specific users method
+@app.get("/get_specific_users", description="Get specific user details")
+def get_specific_users(user_id: int):
+    cursor = conn.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM users_table where user_id = %s", (user_id,))
+    records = cursor.fetchall()
+    cursor.close()
+    return {
+        "message": (
+            f"User with user_id {user_id} not found" if not records 
+            #else "User Fetch Successful (user_id is 5)" if records[0]["user_id"] == 5 
+            else "User Fetch Successful"
+        ),
+        "status": bool(records),  # False if records is empty, True otherwise
+        "data": records if records else None
+    }
+
 # Define add user method
 @app.post("/add_users")
 def add_users(name: str = Form(...), role: str = Form(...)):
     cursor = conn.cursor(dictionary=True)
     cursor.execute("INSERT INTO users_table (name, role) VALUES (%s, %s)", (name, role))
+    cursor.execute("select user_id from users_table where name = (%s) and role = (%s)", (name, role))
+    records = cursor.fetchall()
     conn.commit()
     cursor.close()
     return {
         "message": "New User added successfully",
+        "status": True,
+        "user_id": records
+    }
+
+@app.delete("/delete_user", description="Delete a specific user by ID")
+def delete_user(user_id: int):
+    cursor = conn.cursor()
+    
+    # Execute delete SQL command
+    cursor.execute("DELETE FROM users_table WHERE user_id = %s", (user_id,))
+    conn.commit()  # Commit the transaction to apply changes
+    
+    cursor.close()
+    
+    return {
+        "message": "User Deleted Successfully",
         "status": True
     }
+
